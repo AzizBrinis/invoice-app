e37b0866d648de90503925224c8a835fc8bad61d
Aziz brinis <brinis@Azizs-MacBook-Pro.local>
2025-11-11 16:38:30 +0100

    prisma generate

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,13 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   typedRoutes: true,
+  experimental: {
+    outputFileTracingIncludes: {
+      "/(app)": ["./node_modules/.prisma/client"],
+      "/api": ["./node_modules/.prisma/client"],
+      "/": ["./node_modules/.prisma/client"],
+    },
+  },
 };
 
 export default nextConfig;
